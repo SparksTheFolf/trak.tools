94fbf78f379fa0e4e2883208bee0e156130e3094
Nolan <officialstackemup@gmail.com>
2025-06-16 15:46:53 -0400

    Update

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -3,13 +3,12 @@
     <div class="container">
       <div class="row">
         <div class="col-md-8">
-          <h5>Trak.tools</h5>
-          <p class="text-muted mb-2">
-            Trak.tools is an open-source project to make transit data usable for everyone.
+          <h5>Trak.Tools</h5>
+          <p class="mb-2">
+            Trak.Tools powered by GTFS, Is an <a href="https://github.com/SparksTheFolf/trak.tools">open-source</a> project to make transit data usable for everyone.
           </p>
-          <p class="text-muted mb-0">
-            <i class="bi bi-lightning-charge me-1"></i>
-            Powered by MBTA API
+          <p>
+            Created by <a href="NolanTrapp.com" class="text-light text-decoration-none">Nolan Trapp</a> and contributors.
           </p>
         </div>
         <div class="col-md-4 text-md-end">
